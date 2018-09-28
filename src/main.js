10591dce194d2b0c7a96cc20ba0d4a56b5c6af13
SG <sgng1@e.ntu.edu.sg>
2018-09-28 09:47:01 +0100

    solved challenges!

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
+const createdArray = ["1","2", "3", "4", "5"]
+
 function add(a, b) {
   return a + b;
 }
-
-module.exports = add;
+function subtract(a, b){
+  return b-a
+}
+function pusher(array,b){
+  array.push(b)
+  return array
+}
+function popper(array){
+  array.pop()
+  return array
+}
+module.exports = {
+  add: add,
+  subtract: subtract,
+  createdArray: createdArray,
+  pusher: pusher,
+  popper: popper
+}
--- test/main.test.js
+++ test/main.test.js
@@ -1,5 +1,34 @@
-const add = require("../src/main");
+const {add,subtract,createdArray,pusher, popper} = require("../src/main");
 
-test("should be a descriptive message", () => {
+// declare helloArray first before using beforeEach()
+let helloArray 
+beforeEach(() => {
+  helloArray = [1,2,3,4,5]
+});
+
+
+describe("add", function(){
+it.skip("should be add the 2 parameters together", () => {
   expect(add(1, 1)).toEqual(2);
 });
+
+it.skip("should return 4 for 2+2",() => {
+  expect(add(2,2)).toEqual(4)
+})
+})
+test("should be able to do substraction",() => {
+  expect(subtract(2,10)).toEqual(8)
+})
+test("array has length of 5",() => {
+  expect(createdArray).toHaveLength(5)
+}
+)
+it("array should have length of 6 when element added", () => {
+  expect(pusher(helloArray,2)).toHaveLength(6)
+}
+)
+it("array should have a length of 4 after element popped",() => {
+  expect(popper([1,2,3,4,5])).toHaveLength(4)
+}
+)
+test
